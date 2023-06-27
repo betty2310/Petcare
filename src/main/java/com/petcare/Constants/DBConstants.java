a5a9078127721bc53d2a6ba3606b57b62b7554f8
ntkien36 <kiena3k46@gmail.com>
2023-06-27 22:33:11 +0700

    1

--- src/main/java/com/petcare/Constants/DBConstants.java
+++ src/main/java/com/petcare/Constants/DBConstants.java
@@ -5,7 +5,7 @@ public class DBConstants {
 
     public static final String DATABASE = "jdbc:mysql://localhost:3306/petcare";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "03062002";
 
     public static final int ROWS_PER_PAGE = 10;
 }
